eea44854666aa6e33484a7b2250e74ffe4f2240f
Gorky Suquinagua <gsuquinagua@hotmail.es>
2019-11-13 00:16:38 -0500

    wip: Creación de interfaz visual para la entrada de datos.
    La pantalla queda desarrollada hasta cuando se da click en el botón, de verfiicar el vehículo.

---
+++ resources/js/components/PicoPlacaComponent.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <el-container>
+      <el-header class="cabecera">
+        <div class="titulo">PICO AND PLACA PREDICTOR</div>
+      </el-header>
+      <el-main class="cuerpo">
+        <el-card :body-style="{ 'text-align': '-webkit-center' }">
+          <el-tag type="info" size="medium">
+            <label class="step-title">Paso 1: Confirmar la fecha de evaluación</label>
+          </el-tag>
+          <div style="padding-top: 1%; padding-bottom: 1%;">
+            <el-date-picker
+              size="mini"
+              v-model="fecha_validacion"
+              type="datetime"
+              placeholder="Seleccionar fecha y hora"
+            ></el-date-picker>
+          </div>
+          <el-tag type="info" size="medium">
+            <label class="step-title">Paso 2: Confirmar el número de placa vehicular</label>
+          </el-tag>
+          <div style="padding-top: 1%; padding-bottom: 1%;">
+            <visor-placa @placa="ingresandoPlaca"></visor-placa>
+          </div>
+          <div style="padding-top: 1%; padding-bottom: 1%;">
+              <el-button type="primary">Verificar vehículo</el-button>
+          </div>
+        </el-card>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import { Container, Header, Main, Card, Tag, DatePicker, Button } from "element-ui";
+import VisorPlaca from "./VisorPlaca";
+import lang from 'element-ui/lib/locale/lang/es'
+import locale from 'element-ui/lib/locale'
+// configure language
+locale.use(lang)
+export default {
+  name: "pico-placa-component",
+  components: {
+    ElContainer: Container,
+    ElHeader: Header,
+    ElMain: Main,
+    ElCard: Card,
+    ElTag: Tag,
+    ElDatePicker: DatePicker,
+    ElButton: Button,
+    VisorPlaca
+  },
+  props: {},
+  data() {
+    return {
+      fecha_validacion: new Date()
+    };
+  },
+  mounted() {},
+  computed: {},
+  methods: {
+      ingresandoPlaca(placa){
+          console.log("Placa: ", placa);
+      }
+  }
+};
+</script>
+
+<style scoped>
+.cabecera {
+  background-color: #009688;
+}
+
+.titulo {
+  text-align: center;
+  margin-top: 1%;
+  font-size: x-large;
+  color: white;
+}
+
+.step-title {
+  font-size: medium;
+}
+</style>
---
+++ resources/js/components/VisorPlaca.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="placa-container">
+    <el-row class="cabecera-placa"></el-row>
+    <el-row>
+      <el-col>
+        <el-input @input="emitirPlaca" type="text" v-model="placa" class="input-placa" placeholder="NÚMERO DE PLACA"></el-input>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import { Row, Col, Container, Image, Input } from "element-ui";
+export default {
+  name: "visor-placa",
+  components: {
+    ElRow: Row,
+    ElCol: Col,
+    ElContainer: Container,
+    ElImage: Image,
+    ElInput: Input
+  },
+  props: {
+    separador: { type: String },
+    numero_separaciones: { type: Number },
+    expresion_regular: { type: String }
+  },
+  data() {
+    return {
+      placa: ""
+    };
+  },
+  methods:{
+      emitirPlaca(){
+          this.$emit("placa", this.placa);
+      }
+  }
+};
+</script>
+
+<style>
+.cabecera-placa {
+  background-image: url(/img/Imagen_Ant2.png);
+  height: 50px;
+  background-repeat: no-repeat;
+  width: 32%;
+}
+
+.input-placa {
+  width: 32%;
+  text-align-last: center;
+}
+
+.input-placa.el-input .el-input__inner {
+  text-align: center;
+  height: 130px !important;
+  font-size: 50px;
+}
+</style>
